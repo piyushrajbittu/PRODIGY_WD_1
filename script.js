731b89d80501e517b199a59d94d11a8c04583bb8
Piyush Raj Bittu <116006898+piyushrajbittu@users.noreply.github.com>
2023-10-11 12:18:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const learnMoreButton = document.querySelector(".btn");
+
+    learnMoreButton.addEventListener("click", function () {
+        alert("Thank you for your interest!");
+    });
+});
